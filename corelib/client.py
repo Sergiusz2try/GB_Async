1120eec9c97c2688ff3cd7f5505b3527b27e6cfe
Sergey <kuchko.02@gmail.com>
2023-03-10 18:34:22 +0300

    Task 1, 2: Select

--- corelib/client.py
+++ corelib/client.py
@@ -1,4 +1,7 @@
+import json
 from socket import *
+import time
+from corelib.errors import ReqFieldMissingError, ServerError
 from corelib.user import User
 import sys
 import datetime
@@ -9,33 +12,48 @@ from corelib.decos import log
 
 def run(args, options_file):
     conf = get_options(args, options_file)
+
+    LOG.info(
+        f'Start client with: server address: {conf["DEFAULT"]["HOST"]}, '
+        f'port: {conf["DEFAULT"]["PORT"]}')
+    
     try:
-        sock = create_connection((conf["DEFAULT"]["HOST"], conf["DEFAULT"]["PORT"]), 5)
-    except error as err:
-        LOG.error(f"Connection error: {err}")
-        sys.exit(2)
-    print("Create socket connection...")
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.connect(conf["DEFAULT"]["HOST"], conf["DEFAULT"]["PORT"])
+        sock.sendall(jim.pack(create_presence()))
+        answer = process_response_ans(jim.unpack(sock.recv(1024)))
+        LOG.info(f"Connected to server with: server answer: {answer}")
+        print("Connected to the server.")
+    except json.JSONDecodeError:
+        LOG.error("Failed unpack Json message")
+        sys.exit(1)
+    except ServerError as err:
+        LOG.error(f"Failed, server error: {err}")
+        sys.exit(1)
+    except ReqFieldMissingError as err:
+        LOG.error(f"In server answer missing mandated field: {err}")
+        sys.exit(1)
+    except ConnectionRefusedError as err:
+        LOG.critical(f"Connection refused: {err}")
+        sys.exit(1)
+    else:
+        client_mode = input("Input your mode ('send' or 'listen'): ")
 
-    try:
-        msg = auth()
-        sock.sendall(msg)
-        LOG.info("Send message...")
-    except sock.timeout:
-        LOG.error("Send data timeout.")
-    except socket.error as err:
-        LOG.critical("Send data error: ", err)
+        if client_mode == "send":
+            try:
+                sock.send(jim.pack(create_message(sock)))
+            except (ConnectionResetError, ConnectionError, ConnectionAbortedError):
+                LOG.error(f"Connection with server failed")
+                sys.exit(1)
 
-    while True:
-        try:
-            msg = sock.recv(1024)
-            LOG.info(jim.unpack(msg))
-            break
-        except error as err:
-            LOG.critical("Server error: ", err)
-            break
+        if client_mode == 'listen':
+                try:
+                    message_from_server(jim.unpack(sock.recv(1024)))
+                except (ConnectionResetError, ConnectionError, ConnectionAbortedError):
+                    LOG.error(f'Connection with server was lose.')
+                    sys.exit(1)
 
-    sock.close()
-    print("Client close...")
+
 
 
 @log
@@ -55,6 +73,75 @@ def get_options(args, options_file):
             options["DEFAULT"]["PORT"] = opt[1]
 
     return options
+
+
+@log
+def message_from_server(message):
+    """Function - received messages from another clients, sended to server"""
+    if "action" in message and message["action"] == "message" and \
+            "sender" in message and "mess_text" in message:
+        print(f'Received message from client '
+            f'{message["sender"]}:\n{message["mess_text"]}')
+        LOG.info(f'Received message from client '
+                    f'{message["sender"]}:\n{message["mess_text"]}')
+    else:
+        LOG.error(f'Received incorrect message from server: {message}')
+
+
+@log
+def create_message(sock, account_name="Guest"):
+    """Function - create message for sending to server"""
+    msg = input("Input your message or !!! for exit.")
+
+    if msg == "!!!":
+        sock.close()
+        LOG.info("Exit at the client's command.")
+        print("Thank's, goodbye!")
+        sys.exit(0)
+    
+    msg_dict = {
+        "action": "message",
+        "time": time.isoformat(),
+        "account_name": account_name,
+        "mess_text": msg, 
+    }
+    LOG.debug(f"Created dict message: {msg_dict}")
+
+    return msg_dict
+
+
+@log
+def create_presence(account_name="Guest"):
+    """
+    Generating request about presence of client
+    Args:
+        account_name (str, optional): _description_. Defaults to "Guest".
+    """
+    out = {
+        "action": "presence",
+        "time": time.isoformat(),
+        "user": {
+            "account_name": account_name,
+        },
+    }
+    LOG.debug(f"Created presence message for client {account_name}")
+
+    return out
+
+
+@log
+def process_response_ans(message):
+    """
+    Function received server answer of message about presence,
+    return 200 if all ОК or generate exception as error
+    """
+    LOG.debug(f'Received message from server: {message}')
+    if "response" in message:
+        if message["response"] == 200:
+            return '200 : OK'
+        elif message["response"] == 400:
+            raise ServerError(f'400 : {message["error"]}')
+    raise ReqFieldMissingError("response")
 
 
 @log
--- corelib/server.py
+++ corelib/server.py
@@ -1,4 +1,6 @@
+import select
 from socket import *
+import time
 from corelib import jim, config
 from logs.server_log_config import LOG
 from corelib.decos import log
@@ -13,28 +15,68 @@ def run(args, options_file):
     conf = get_options(args, options_file)
     sock = socket(AF_INET, SOCK_STREAM)
     sock.bind((conf["DEFAULT"]["HOST"], conf["DEFAULT"]["PORT"]))
+    sock.settimeout(0.5)
+
+    LOG.info(
+        f'Запущен сервер, порт для подключений: {conf["DEFAULT"]["PORT"]}, '
+    )
+
     print("Create server...")
     sock.listen(5)
     print("Server listening...")
 
-    while True:
-        conn, _ = sock.accept()
-        conn.settimeout(5)
-        with conn:
-            while True:
-                try:
-                    msg = conn.recv(1024)
-                    LOG.info(jim.unpack(msg))
-                    conn.sendall(response_200())
-                except error as err:
-                    LOG.critical(f"Error: {err}")
-                    break
-                if not msg:
-                    break
-        break
+    # Creating clients and messages variables
+    clients = []
+    messages = []
 
-    sock.close()
-    print("Server close...")
+    while True:
+        try:
+            conn, addr = sock.accept()
+        except OSError:
+            pass
+        else:
+            LOG.info(f"Connected with: {addr}")
+            clients.append(conn)
+        
+        # Created data variables
+        recv_data = []
+        write_data = []
+        err_data = []
+
+        try:
+            if clients:
+                recv_data, write_data, err_data = select.select(clients, clients, [], 0)
+        except OSError:
+            pass
+
+        if recv_data:
+            for client_with_message in recv_data:
+                try:
+                    process_client_message(
+                        jim.unpack(client_with_message.recv(1024)),
+                        messages,
+                        client_with_message
+                        )
+                except:
+                    LOG.info(f'Client {client_with_message.getpeername()} '
+                            f'disconnect from server.')
+                    
+        if messages and write_data:
+            msg = {
+                "action": "message",
+                "sender": messages[0][0],
+                "time": time.isoformat(),
+                "mess_text": messages[0][1],
+            }
+
+            del messages[0]
+            for writing_client in write_data:
+                try:
+                    writing_client.send(jim.pack(msg))
+                except:
+                    LOG.info(f'Client {client_with_message.getpeername()} '
+                            f'disconnect from server.')
+                    clients.remove(writing_client)
 
 
 @log
@@ -57,10 +99,44 @@ def get_options(args, options_file):
 
 
 @log
+def process_client_message(message, messages_list, client):
+    """
+    Message handler from clients, accepts a dictionary - a message from the client,
+    checks the correctness, sends a response dictionary to the client with the result of the reception.
+    :param message:
+    :param messages_list:
+    :param client:
+    :return:
+    """
+
+    LOG.debug(f"Received message from client: {message}")
+    if "action" in message and message["action"] == "presence" and "time" in message \
+            and "user" in message and message["user"]["account_name"] == "Guest":
+        client.send(response_200())
+        return
+    elif "action" in message and message["action"] == "message" and \
+            "time" in message and "mess_text" in message:
+        messages_list.append(message["account_name"], message["text"])
+    else:
+        client.send(response_400())
+        return
+
+
+@log
 def response_200():
     msg = {
         "response": 200,
         "alert": "OK"
+    }
+
+    return jim.pack(msg)
+
+
+@log
+def response_400():
+    msg = {
+        "response": 400,
+        "alert": "Bad request"
     }
 
     return jim.pack(msg)
